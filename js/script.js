2745eb2457bb827790fdeeb7a2360944dadcd973
Dylan McGee <dtmcgee@syr.edu>
2021-06-29 16:36:54 -0400

    Add Focus on "Name" input and add "Other Job Role" input if Job Role = "Other"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('test');
+
+const name = document.getElementById("name");
+name.focus(); // Allows the name tab to be automatically selected
+
+const title = document.getElementById('title');
+const otherJobRole = document.getElementById('other-job-role');
+
+//Remove 'Other Job Role' field if the user selects "Other" as a Job Role
+title.addEventListener('change', (e) => {
+    if (e.target.value !== 'other') {
+        otherJobRole.style.display = 'none';
+    } else {
+        otherJobRole.style.display = 'block';
+    }
+});
