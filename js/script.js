e13e338d84b5c82c919515c7b5f499196d6424f4
Dylan McGee <dtmcgee@syr.edu>
2021-07-02 17:06:28 -0400

    Completed "Register for Activities" Section

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ title.addEventListener('change', (e) => {
     }
 });
 
-
+/******* T-SHIRT INFO  ******/
  const shirtDesign = document.getElementById('design');
  const shirtColor = document.getElementById('color');
  const shirtColorChildren = shirtColor.children; 
@@ -36,4 +36,29 @@ title.addEventListener('change', (e) => {
      }
     });
 
+/******* REGISTER FOR ACTIVITIES SECTION  ******/
 
+const fieldSet = document.getElementById('activities');
+//console.log(fieldSet);
+const checkBoxes = fieldSet.querySelectorAll('input');
+//console.log(checkBoxes);
+const totalElement = document.getElementById('activities-cost');
+const totalElementNoDollarSign = totalElement.textContent.replace(/[$,.\s]0/g, '');
+console.log(totalElement);
+fieldSet.addEventListener('change', (e) => {
+    let total = 0;
+
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked == true) {
+            //console.log(checkBoxes[i]);
+            let price = parseInt(checkBoxes[i].getAttribute('data-cost'));
+            //console.log(price);
+            total = (total + price);
+            //console.log(total);
+            totalElement.innerText = `$${total}`;
+        }
+    }
+});
+
+const paymentMehtod = document.querySelectorAll('.payment');
+//paymentMehtod.selected = 'selected';
