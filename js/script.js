96e0c3b83a1ecd1840c27f798eaf648eca37a8ad
Dylan McGee <dtmcgee@syr.edu>
2021-07-06 23:22:55 -0400

    Completed Accessibility Work

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
-console.log('test');
-
-
 const nameInput = document.getElementById("name");
 nameInput.focus(); // Allows the name tab to be automatically selected
 
 const title = document.getElementById('title');
 const otherJobRole = document.getElementById('other-job-role');
 
-//Remove 'Other Job Role' field if the user selects "Other" as a Job Role
+//Add 'Other Job Role' field if the user selects "Other" as a Job Role
 otherJobRole.style.display = 'none';
 title.addEventListener('change', (e) => {
     if (e.target.value !== 'other') {
@@ -18,36 +15,33 @@ title.addEventListener('change', (e) => {
 });
 
 /******* T-SHIRT INFO  ******/
- const shirtDesign = document.getElementById('design');
- const shirtColor = document.getElementById('color');
- const shirtColorChildren = shirtColor.children; 
- shirtColor.disabled = true;
+const shirtDesign = document.getElementById('design');
+const shirtColor = document.getElementById('color');
+const shirtColorChildren = shirtColor.children;
+shirtColor.disabled = true;
 
- shirtDesign.addEventListener('change', (e) => {
-     shirtColor.disabled = false;
-     for (let i = 1; i < shirtColorChildren.length; i++) {
-         const theme = e.target.value;
-         const dataTheme = shirtColorChildren[i].getAttribute('data-theme');
-          if (theme === dataTheme) {
-            shirtColorChildren[i].selected = true;         
+shirtDesign.addEventListener('change', (e) => {
+    shirtColor.disabled = false;
+    for (let i = 1; i < shirtColorChildren.length; i++) {
+        const theme = e.target.value;
+        const dataTheme = shirtColorChildren[i].getAttribute('data-theme');
+        if (theme === dataTheme) {
+            shirtColorChildren[i].selected = true;
             shirtColorChildren[i].hidden = false;
-          } else {
-            shirtColorChildren[i].selected = false;         
+        } else {
+            shirtColorChildren[i].selected = false;
             shirtColorChildren[i].hidden = true;
-         };
-     }
-    });
+        };
+    }
+});
 
 /******* REGISTER FOR ACTIVITIES SECTION  ******/
-
 const fieldSet = document.getElementById('activities');
-//console.log(fieldSet);
 const checkBoxes = fieldSet.querySelectorAll('input');
-//console.log(checkBoxes);
 const totalElement = document.getElementById('activities-cost');
 const totalElementNoDollarSign = totalElement.textContent.replace(/[$,.\s]0/g, '');
-//console.log(totalElement);
 let total = 0;
+
 fieldSet.addEventListener('change', (e) => {
     if (e.target.checked) {
         total += parseInt(e.target.dataset.cost);
@@ -63,12 +57,11 @@ const paymentTypes = paymentMehtod.getElementsByTagName('option');
 const creditCardOption = paymentMehtod.getElementsByTagName('option')[1];
 creditCardOption.selected = 'selected';
 const creditCard = document.getElementById('credit-card');
-const paypal  = document.getElementById('paypal');
+const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
 paypal.style.display = 'none';
 bitcoin.style.display = 'none';
 paymentMehtod.addEventListener('change', (e) => {
-    //console.log(paymentTypes);
     for (let i = 1; i < paymentTypes.length; i++) {
         if (paymentTypes[1].selected) {
             creditCard.style.display = 'block';
@@ -78,12 +71,12 @@ paymentMehtod.addEventListener('change', (e) => {
             paypal.style.display = 'block';
             creditCard.style.display = 'none';
             bitcoin.style.display = 'none';
-        } else  {
+        } else {
             bitcoin.style.display = 'block';
             creditCard.style.display = 'none';
             paypal.style.display = 'none';
         }
-        }
+    }
 
 });
 
@@ -93,38 +86,21 @@ const zipCode = document.getElementById('zip');
 const cvv = document.getElementById('cvv');
 const form = document.querySelector('form');
 const submit = document.querySelector('button');
-console.log(form);
 
-//ENDED HERE 7/5 CHECL ASTERICK CLSS AND ASSIGN ALL TITLES FOR REQUIRED FIELDS
 const requiredTitles = document.querySelectorAll('.asterisk');
 form.addEventListener('submit', (e) => {
     checkAll();
     if (checkAll() === true) {
-        window.location.reload();
-    } else{
+       window.location.reload();
+    } else {
         e.preventDefault();
     }
-    
-
 });
-    
-    
-//     if (paymentTypes[1].selected) {
-//         if (creditCardRegex() && zipCodeRegex() && cvvTest()) {
-//             console.log('credit card stuff worked');
-//         } else {
-//             console.log('credit card failed');
-//         }
-//     }
-//     if (total === 0){
-//         //activitesTitle.classList.add('not-valid');
-//     }
-// });
 
 //REGEX FUNCTIONS
 const nameRegex = () => {
     const nameValue = nameInput.value;
-   // https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+    // https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
     return /^[a-z ,.'-]+$/i.test(nameValue);
 }
 
@@ -148,14 +124,12 @@ const cvvRegex = () => {
 
 const checkAll = () => {
 
-    // for (let i =1; i < 6; i++){
-
-    // }
     if (!nameRegex()) {
         requiredTitles[1].parentElement.classList.add('not-valid');
         requiredTitles[1].nextElementSibling.nextElementSibling.classList.remove('hint');
     } else {
         requiredTitles[1].parentElement.classList.remove('not-valid');
+        requiredTitles[1].parentElement.classList.add('valid');
         requiredTitles[1].nextElementSibling.nextElementSibling.classList.add('hint');
     }
 
@@ -164,11 +138,13 @@ const checkAll = () => {
         requiredTitles[2].nextElementSibling.nextElementSibling.classList.remove('hint');
     } else {
         requiredTitles[2].parentElement.classList.remove('not-valid');
+        requiredTitles[2].parentElement.classList.add('valid');
         requiredTitles[2].nextElementSibling.nextElementSibling.classList.add('hint');
     }
     if (total === 0) {
         requiredTitles[3].parentElement.classList.add('not-valid');
     } else {
+        requiredTitles[3].parentElement.classList.add('valid');
         requiredTitles[3].parentElement.classList.remove('not-valid');
     }
 
@@ -177,14 +153,16 @@ const checkAll = () => {
         requiredTitles[4].nextElementSibling.nextElementSibling.classList.remove('hint');
     } else {
         requiredTitles[4].parentElement.classList.remove('not-valid');
+        requiredTitles[4].parentElement.classList.add('valid');
         requiredTitles[4].nextElementSibling.nextElementSibling.classList.add('hint');
     }
 
-    if (!zipCodeRegex()) {
+    if (!creditCardRegex()) {
         requiredTitles[5].parentElement.classList.add('not-valid');
         requiredTitles[5].nextElementSibling.nextElementSibling.classList.remove('hint');
     } else {
         requiredTitles[5].parentElement.classList.remove('not-valid');
+        requiredTitles[5].parentElement.classList.add('valid');
         requiredTitles[5].nextElementSibling.nextElementSibling.classList.add('hint');
     }
 
@@ -193,16 +171,23 @@ const checkAll = () => {
         requiredTitles[6].nextElementSibling.nextElementSibling.classList.remove('hint');
     } else {
         requiredTitles[6].parentElement.classList.remove('not-valid');
+        requiredTitles[6].parentElement.classList.add('valid');
         requiredTitles[6].nextElementSibling.nextElementSibling.classList.add('hint');
     }
-    
+    if (nameRegex() && emailRegex() && total != 0 && creditCardRegex() && creditCardRegex() && cvvRegex()) {
+        return true
+    } else {
+        false
+    }
 }
 
-for (let i = 3; i < 7; i++) {
-    fieldSet.addEventListener('focus', (e) => {
-        checkBoxes[i].parentElement.classList.add('focus');
+//Adds Accessability through tabbing for Activity options  
+const activityOptions = document.querySelectorAll("#activities input");
+for (let i = 0; i < activityOptions.length; i++) {
+    activityOptions[i].addEventListener('focus', (e) => {
+        activityOptions[i].parentElement.classList.add('focus');
     });
-   fieldSet.addEventListener('blur', (e) => {
-        checkBoxes[i].parentElement.classList.remove('focus');
+    activityOptions[i].addEventListener('blur', (e) => {
+        activityOptions[i].parentElement.classList.remove('focus');
     });
 };
